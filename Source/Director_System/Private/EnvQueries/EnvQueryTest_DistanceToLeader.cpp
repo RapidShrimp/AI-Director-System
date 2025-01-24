f81b1cdf95093b4219b03356a84a6cd3194305ad
RapidAdam1 <adamshrimpton04@outlook.com>
2025-01-24 20:51:23 +0000

    Some Changes
    Ai now moves semi like a grouo

---
+++ Source/Director_System/Private/EnvQueries/EnvQueryTest_DistanceToLeader.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnvQueries/EnvQueryTest_DistanceToLeader.h"
+
+void UEnvQueryTest_DistanceToLeader::RunTest(FEnvQueryInstance& QueryInstance) const
+{
+	
+}
