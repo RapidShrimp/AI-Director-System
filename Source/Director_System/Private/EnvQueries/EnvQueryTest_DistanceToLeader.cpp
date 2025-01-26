0c6145d893f07eed59a4422cd51e93e03a3ce7a0
RapidAdam1 <adamshrimpton04@outlook.com>
2025-01-26 17:36:44 +0000

    Formation Test Failure

--- Source/Director_System/Private/EnvQueries/EnvQueryTest_DistanceToLeader.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "EnvQueries/EnvQueryTest_DistanceToLeader.h"
-
-void UEnvQueryTest_DistanceToLeader::RunTest(FEnvQueryInstance& QueryInstance) const
-{
-	
-}
